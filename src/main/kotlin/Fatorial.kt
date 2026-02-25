fcfa57bbf5cf43937b417cb42298391d515135d5
Gabriel <gabrielfortaleza4@gmail.com>
2026-02-25 10:39:45 -0300

    fatorial

--- src/main/kotlin/Fatorial.kt
+++ src/main/kotlin/Fatorial.kt
@@ -2,7 +2,7 @@ fun main() {
     print("Digite o numero para fatorar: ")
     val n = readLine()!!.toInt()
 
-    var fatorial = 1
+    var fatorial = 1.toLong()
     var i = n
 
     while (i >= 1){
